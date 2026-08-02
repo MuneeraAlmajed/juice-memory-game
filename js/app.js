9fa8666e178793d7947c87e5ea9fb412e8bb0b9c
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-08-02 09:45:57 +0300

    js file

--- js/app.js
+++ js/app.js
@@ -16,4 +16,11 @@
 
 
 
+//this is the last line of code should be existed
+const handleMove = (event) => {
 
+    //Logic for the event handler, update state variables, etc
+
+    //Render data to the DOM
+    render();
+}
