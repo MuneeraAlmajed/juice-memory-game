533c97308c07313e5db0c1af5fc82eba1e03b622
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-08-02 15:31:47 +0300

    updated code

--- js/app.js
+++ js/app.js
@@ -1,26 +1,52 @@
 /*-------------- Constants -------------*/
 
+// Ingredients available in the game
+const ingredients = [
+    "Orange",
+    "Strawberry",
+    "Apple",
+    "Banana",
+    "Blueberry",
+    "Honey",
+    "Lemon",
+    "Milk",
+    "Mango",
+    "Watermelon",
+    "Kiwi",
+    "Ice"
+];
+
 
 /*---------- Variables (state) ---------*/
 
+let correctIngredients = [];
+let selectedIngredients = [];
+let timeLeft = 5;
 
-/*----- Cached Element References  -----*/
+
+/*----- Cached Element References -----*/
+
+const startButton = document.querySelector("#start-btn");
+const ingContainer = document.querySelector("#ingredients");
+const submitButton = document.querySelector("#submit-btn");
+const playAgainButton = document.querySelector("#play-again-btn");
+const instructions = document.querySelector("#instructions");
+const message = document.querySelector("#message");
+const timer = document.querySelector("#timer");
 
 
 /*-------------- Functions -------------*/
 
-
 /*----------- Event Listeners ----------*/
 
 
+// Start button
+startButton.addEventListener("click", startGame);
 
 
+// Submit button
+submitButton.addEventListener("click", submitAnswer);
 
-//this is the last line of code should be existed
-const handleMove = (event) => {
 
-    //Logic for the event handler, update state variables, etc
-
-    //Render data to the DOM
-    render();
-}
+// Play Again button
+playAgainButton.addEventListener("click", playAgain);
