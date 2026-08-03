46f16f262c4943913ed5314e4c273a14eeda1946
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-08-03 12:29:17 +0300

    updated js

--- js/app.js
+++ js/app.js
@@ -19,8 +19,12 @@ const ingredients = [
 
 /*---------- Variables (state) ---------*/
 
+let playerChoice;
+let random;
+let fiveIngredients =[];
 let correctIngredients = [];
 let selectedIngredients = [];
+let matchResult; //win or lose
 let timeLeft = 5;
 
 
@@ -36,22 +40,129 @@ const timer = document.querySelector("#timer");
 
 
 /*-------------- Functions -------------*/
+
 //start game
 function startGame(){
     console.log("the game is started");
+
+    //create 5 random ingredients
+    getRandomIng();
+
+    timeLeft = 5;
+    timer.textContent = "Time Left: "+timeLeft;
+
+    const countdown = setinterval(()=>{
+        timeLeft--;
+
+        timer.textContent="Time Left: "+ timeLeft;
+
+        if(timeLeft===0){
+            clearInterval(countdown);
+
+            ingContainer.innerHTML = "";
+        }
+    })
+
+    //show ingredients for 5 seconds
+    setTimeout(() => {
+
+        //hide them
+        ingContainer.innerHTML = "";
+
+        console.log("ingredients are hidden!");
+
+        //display all the ingredients for the player
+        for(const allIngredients of ingredients){
+            const ingButton = document.createElement("button");
+
+            ingButton.textContent = allIngredients;
+            ingButton.id = allIngredients;
+
+            ingButton.addEventListener("click",getPlayerChoice);
+
+            ingContainer.appendChild(ingButton);
+        }
+        console.log("Choose your ingredients!");
+    },5000);
 }
 
 //play again
 function playAgain(){
     console.log("you can play again");
+
+//clear the arrays
+selectedIngredients.innerHTML = "";
+correctIngredients.innerHTML = "";
+ingContainer.innerHTML = "";
+
+ingContainer.innerHTML = "";
+
 }
+
 
 //make juice
 function makeJuice(){
     console.log("Juice is made!");
-}
-/*----------- Event Listeners ----------*/
 
+    console.log("correct answer: ",fiveIngredients);
+    console.log("player answer: ",selectedIngredients);
+
+    const isCorrect = fiveIngredients.length === selectedIngredients.length && fiveIngredients.every(ingredient => selectedIngredients.includes(ingredient));
+
+    if(isCorrect){
+        message.textContent = "You win!";
+    }else{
+        message.textContent = "You lose!";
+    }
+}
+
+//player choice
+const getPlayerChoice = (event) => {
+    playerChoice = event.target.id;  
+
+    //store player selection
+    selectedIngredients.push(playerChoice);
+
+    console.log("player selected: ", playerChoice);
+    console.log("All selected ingredient: ",selectedIngredients);
+};
+
+//computer random ingredients
+const getRandomIng = () =>{
+
+//to choose 5 ingredients from the array randomly
+    for(i = 0; i<5;i++){
+    const randomIndex = Math.floor(Math.random() * ingredients.length);
+    randomIng = ingredients[randomIndex];
+    console.log("ingredinets: " +randomIng);
+
+    // check if ingredients is already selected
+    if(!fiveIngredients.includes(randomIng)){
+        fiveIngredients.push(randomIng);
+    }else{
+        //if duplicate try again
+        i--;
+    }
+}
+//display the correct ingredients
+    for (const ing of fiveIngredients){
+        const ingButton = document.createElement("button");
+
+        ingButton.textContent = ing;
+        ingButton.id = ing;
+
+        //store player choice when clicked
+        ingButton.addEventListener("click",getPlayerChoice);
+
+        
+    ingContainer.appendChild(ingButton);
+    }
+    console.log("correct answer: ", fiveIngredients);
+}
+
+
+
+/*----------- Event Listeners ----------*/
 
 // Start button
 startButton.addEventListener("click", startGame);
@@ -61,4 +172,7 @@ playAgainButton.addEventListener("click", playAgain);
 
 //make juice button or submit button
 submitButton.addEventListener("click", makeJuice);
+
+
+
 
