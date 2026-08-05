f99ff5a214502b9f8b3f031945552edf148eff5e
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-08-05 17:21:22 +0300

    latest

--- js/app.js
+++ js/app.js
@@ -295,6 +295,13 @@ function addIngredient(ingredient) {
 //render
 function render(){
     addIngredient(playerChoice);
+
+    // keep the selected card highlighted
+    const selectedButton = document.getElementById(playerChoice);
+
+    if(selectedButton){
+        selectedButton.classList.add("selected");
+    }
 }
 
 
@@ -314,8 +321,6 @@ const handleMove = (event) => {
 
     playerChoice = event.target.id;
     selectedIngredients.push(playerChoice);
-
-    event.target.classList.add("selected");
 
     console.log("player selected: ", playerChoice);
     console.log("All selected ingredient: ", selectedIngredients);
